02cefb08ba35bfadf1c3d43359d6397f1f318f7f
Vishnu <codemaker2015@gmail.com>
2023-02-19 19:43:47 +0530

    initial commit

---
+++ Assets/Scripts/SystemInfoScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SystemInfoScript : MonoBehaviour
+{
+    [SerializeField]
+    Text info;
+    string infoString = "";
+    // Start is called before the first frame update
+    void Start()
+    {
+       infoString += "Battery Level: " + SystemInfo.batteryLevel + "\n";
+       infoString += "Battery Status: " + SystemInfo.batteryStatus + "\n";
+       infoString += "Device Model: " + SystemInfo.deviceModel + "\n";
+       infoString += "Device Name: " + SystemInfo.deviceName + "\n";
+       infoString += "Device Type: " + SystemInfo.deviceType + "\n";
+       infoString += "Device Unique Identifier: " + SystemInfo.deviceUniqueIdentifier + "\n";
+       infoString += "Operating System: " + SystemInfo.operatingSystem + "\n";
+       infoString += "Processor Type: " + SystemInfo.processorType + "\n";
+       infoString += "Memory Size: " + SystemInfo.systemMemorySize + "\n";
+       infoString += "Acceleromer Support: " + SystemInfo.supportsAccelerometer + "\n";
+       infoString += "Gyroscope Support: " + SystemInfo.supportsGyroscope + "\n";
+       infoString += "Vibration Support: " + SystemInfo.supportsVibration + "\n";
+       info.text = infoString;
+
+       // For more: https://docs.unity3d.com/ScriptReference/SystemInfo.html
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
